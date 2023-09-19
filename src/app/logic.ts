868c887ff3149bc9b422ffe27623bc1259547269
Macumba45 <gonzalolovo@gmail.com>
2023-09-19 15:31:12 +0200

    fix: seat in each travel done

--- src/app/logic.ts
+++ src/app/logic.ts
@@ -1,29 +1,130 @@
-// export const logic = () => {
-//     const route = 'ALGECEUT'; // Obtén el valor de route desde el frontend
-//     const time = '2023-09-21'; // Obtén el valor de time desde el frontend
+import { useState } from 'react'
+import { Departure, Seats } from './types'
 
-//     const API_URL = 'http://localhost:3000/departures?route=ALGECEUT&time=2023-09-25';
+export const logic = () => {
+    const date = new Date()
+    const [showOffers, setShowOffers] = useState<boolean>(false)
+    const [route, setRoute] = useState<string>('ALGECEUT')
+    const [time, setTime] = useState<string>(date.toISOString().split('T')[0])
+    const [adults, setAdults] = useState<number>(1)
+    const [children, setChildren] = useState<number>(0)
+    const [infants, setInfants] = useState<number>(0)
+    const [dataTravels, setDataTravels] = useState<Departure[]>([])
+    const [dataSeats, setDataSeats] = useState<Seats[]>([])
+    const [showSeats, setShowSeats] = useState<boolean>(false)
+    const API_URL = `http://localhost:3000/departures?route=${route}&time=${time}`
+    const API_URL_DIRECT = `https://tadpole.clickferry.app/departures?route=${route}&time=${time}`
+    const API_URL_ACOMMODATION = `http://localhost:3000/departures/accomodations?route=${route}&time=${time}&adults=${adults}&children=${children}&babies=${infants}`
 
-//     const getDepartures = async () => {
+    const routes = [
+        {
+            value: 'ALGECEUT',
+            from: 'Algeciras',
+            to: 'Ceuta',
+        },
+        {
+            value: 'CEUTALGE',
+            from: 'Ceuta',
+            to: 'Algeciras',
+        },
+    ]
 
-//         try {
-//             const response = await fetch(API_URL, {
-//                 method: "GET",
-//             });
-//             if (response.ok) {
-//                 const data = await response.json();
-//                 console.log(data);
-//             } else {
-//                 throw new Error("Error en la petición");
-//             }
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     };
+    const getDepartures = async () => {
+        try {
+            const response = await fetch(API_URL, {
+                method: 'GET',
+            })
+            if (response.ok) {
+                const data = await response.json()
+                setDataTravels(data)
+            } else {
+                throw new Error('Error en la petición')
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    const [seatsByTravel, setSeatsByTravel] = useState<{
+        [key: string]: Seats[]
+    }>({})
 
-//     return {
-//         getDepartures,
-//     };
-// };
+    const getSeats = async (selectedTravelTime: any) => {
+        const reservationURL = `http://localhost:3000/departures/accomodations?route=${route}&time=${selectedTravelTime}&adults=${adults}&children=${children}&babies=${infants}`
+        try {
+            const response = await fetch(reservationURL, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            if (response.ok) {
+                const responseData = await response.json()
+                setShowSeats(true)
+                setSeatsByTravel(prevState => ({
+                    ...prevState,
+                    [selectedTravelTime]: responseData,
+                }))
+                console.log('Respuesta JSON:', responseData)
+            } else {
+                throw new Error(`HTTP error! Status: ${response.status}`)
+            }
+        } catch (error) {
+            console.error('Error al realizar la solicitud:', error)
+        }
+    }
 
-// export default logic;
+    const getFinalPrice = async (selectedTravelTime: any, seat: any) => {
+        const seatPrice = `http://localhost:3000/departures/accomodations?route=${route}&time=${selectedTravelTime}&adults=${adults}&children=${children}&babies=${infants}&accommodation=${seat}`
+        try {
+            const response = await fetch(seatPrice, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            if (response.ok) {
+                const responseData = await response.json()
+                console.log('Respuesta JSON:', responseData)
+            }
+        } catch (error) {
+            console.error('Error al realizar la solicitud:', error)
+        }
+    }
+
+    function formatDate(date: any) {
+        const dateObject = new Date(date)
+        const dayOb = dateObject.getDate()
+        const monthOb = new Intl.DateTimeFormat('es-ES', {
+            month: 'short',
+        }).format(dateObject)
+        const formattedDateOb = `${dayOb} ${monthOb}`
+        return formattedDateOb
+    }
+
+    return {
+        showOffers,
+        setShowOffers,
+        route,
+        setRoute,
+        time,
+        setTime,
+        adults,
+        setAdults,
+        children,
+        setChildren,
+        infants,
+        setInfants,
+        dataTravels,
+        getDepartures,
+        getSeats,
+        getFinalPrice,
+        formatDate,
+        routes,
+        showSeats,
+        setShowSeats,
+        dataSeats,
+        seatsByTravel,
+    }
+}
+
+export default logic
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,104 +6,35 @@ import TravelBooking from './componets/TravelBooking'
 import SelectTravel from './componets/SelectTravel'
 import { Departure, Seats } from './types'
 import Header from './componets/Header'
+import logic from './logic'
 
 const Home: FC = () => {
-    const date = new Date()
-    const [showOffers, setShowOffers] = useState<boolean>(false)
-    const [route, setRoute] = useState<string>('ALGECEUT')
-    const [time, setTime] = useState<string>(date.toISOString().split('T')[0])
-    const [adults, setAdults] = useState<number>(1)
-    const [children, setChildren] = useState<number>(0)
-    const [infants, setInfants] = useState<number>(0)
-    const [dataTravels, setDataTravels] = useState<Departure[]>([])
-    const [dataSeats, setDataSeats] = useState<Seats[]>([])
-    const [showSeats, setShowSeats] = useState<boolean>(false)
-    const API_URL = `http://localhost:3000/departures?route=${route}&time=${time}`
-    const API_URL_DIRECT = `https://tadpole.clickferry.app/departures?route=${route}&time=${time}`
-    const API_URL_ACOMMODATION = `http://localhost:3000/departures/accomodations?route=${route}&time=${time}&adults=${adults}&children=${children}&babies=${infants}`
+    const {
+        showOffers,
+        setShowOffers,
+        route,
+        setRoute,
+        time,
+        setTime,
+        adults,
+        setAdults,
+        children,
+        setChildren,
+        infants,
+        setInfants,
+        dataTravels,
+        getDepartures,
+        getSeats,
+        getFinalPrice,
+        formatDate,
+        routes,
+        showSeats,
+        setShowSeats,
+        seatsByTravel,
+        dataSeats,
+    } = logic()
 
-    const routes = [
-        {
-            value: 'ALGECEUT',
-            from: 'Algeciras',
-            to: 'Ceuta',
-        },
-        {
-            value: 'CEUTALGE',
-            from: 'Ceuta',
-            to: 'Algeciras',
-        },
-    ]
-
-    const getDepartures = async () => {
-        try {
-            const response = await fetch(API_URL, {
-                method: 'GET',
-            })
-            if (response.ok) {
-                const data = await response.json()
-                setDataTravels(data)
-            } else {
-                throw new Error('Error en la petición')
-            }
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
-    const getSeats = async (selectedTravelTime: any) => {
-        // Construir la URL con los parámetros actuales
-        const reservationURL = `http://localhost:3000/departures/accomodations?route=${route}&time=${selectedTravelTime}&adults=${adults}&children=${children}&babies=${infants}`
-
-        try {
-            // Realizar la solicitud GET a la URL de reserva
-            const response = await fetch(reservationURL, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            })
-
-            if (response.ok) {
-                const responseData = await response.json()
-                setShowSeats(true)
-                setDataSeats(responseData)
-                console.log('Respuesta JSON:', responseData)
-            } else {
-                throw new Error(`HTTP error! Status: ${response.status}`)
-            }
-        } catch (error) {
-            console.error('Error al realizar la solicitud:', error)
-        }
-    }
-
-    const getFinalPrice = async (selectedTravelTime: any, seat: any) => {
-        const seatPrice = `http://localhost:3000/departures/accomodations?route=${route}&time=${selectedTravelTime}&adults=${adults}&children=${children}&babies=${infants}&accommodation=${seat}`
-        try {
-            const response = await fetch(seatPrice, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            })
-            if (response.ok) {
-                const responseData = await response.json()
-                console.log('Respuesta JSON:', responseData)
-            }
-        } catch (error) {
-            console.error('Error al realizar la solicitud:', error)
-        }
-    }
-
-    function formatDate(date: any) {
-        const dateObject = new Date(date)
-        const dayOb = dateObject.getDate()
-        const monthOb = new Intl.DateTimeFormat('es-ES', {
-            month: 'short',
-        }).format(dateObject)
-        const formattedDateOb = `${dayOb} ${monthOb}`
-        return formattedDateOb
-    }
+    console.log(seatsByTravel)
 
     useEffect(() => {
         getDepartures()
@@ -149,14 +80,10 @@ const Home: FC = () => {
                             adults={adults}
                             children={children}
                             infants={infants}
+                            data={seatsByTravel[travel.time.slice(0, -1)]}
                             onSelected={() =>
                                 getSeats(travel.time.slice(0, -1))
                             }
-                            isOpen={showSeats}
-                            data={dataSeats.map(seat => ({
-                                code: seat.code,
-                                name: seat.name,
-                            }))}
                         />
                     ))}
             </CardContainer>
